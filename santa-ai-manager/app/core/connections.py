d3ad0c63a3a66ec985fbe87bc2e9f745c6d0fca0
JunOnJuly-Project <bookandpapers717@gmail.com>
2025-12-19 16:30:12 +0900

    Update connections.py

--- santa-ai-manager/app/core/connections.py
+++ santa-ai-manager/app/core/connections.py
@@ -1,10 +1,17 @@
-from redis.asyncio import Redis, ConnectionPool # asyncio용으로 변경
+from redis.asyncio import Redis, ConnectionPool
 from app.core.config import settings
+import ssl
 
+# 연결 풀 생성
 pool = ConnectionPool(
-    host=settings.REDIS_HOST, 
-    port=settings.REDIS_PORT, 
-    decode_responses=True
+    host=settings.REDIS_HOST,
+    port=settings.REDIS_PORT,
+    decode_responses=True,
+    ssl=True,               
+    ssl_cert_reqs=None,      
+    socket_timeout=5.0,       
+    socket_keepalive=True     
 )
 
+# 비동기 클라이언트 생성
 redis_client = Redis(connection_pool=pool)
