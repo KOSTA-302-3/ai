c1dad786f6a3fdc9d67170153dcbe45aff65c4a7
JunOnJuly_Project <wlgnstjd0717@gmail.com>
2025-12-29 11:52:51 +0900

    fix feedback

--- santa-ai-manager/app/models/post.py
+++ santa-ai-manager/app/models/post.py
@@ -5,4 +5,4 @@ class Post(Base):
     __tablename__ = "posts" # 실제 RDS 테이블 이름
 
     post_id = Column(Integer, primary_key=True, index=True)
-    level = Column(Integer, default=1)
+    level = Column("post_level", Integer, default=1)
